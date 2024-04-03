8d0967ca45b1c9de8d33abd95a2e6a6befc14ef0
GRA <gr-greta-2024@longtime.fr>
2024-04-03 19:11:18 +0200

    [improve] Ajoute un minimum de navigation.

---
+++ view/_header.php
@@ -0,0 +1,14 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/ctrl/navire-list.php">Navires</a></li>
+            <li><a href="/ctrl/marin-list.php">Marins</a></li>
+            <li><a href="/ctrl/add-marin-display.php">Marin +</a></li>
+            <li><a href="/ctrl/activite-list.php">Activités</a></li>
+            <li><a href="/ctrl/add-activite-display.php">Activité +</a></li>
+
+        </ul>
+    </nav>
+
+    <h1><?= $pageTitle ?></h1>
+</header>
--- view/navire.php
+++ view/navire.php
@@ -1,19 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href="/asset/css/style.css">
 </head>
+
 <body>
 
-<main>
-    <ul>
-        <li>id : <?= $listNavire[0]['id'] ?></li>
-        <li>numeroIMO : <?= $listNavire[0]['numeroIMO'] ?></li>
-        <li>nom : <?= $listNavire[0]['nom'] ?></li>
-        <li>idTypeNavire : <?= $listNavire[0]['idTypeNavire'] ?></li>
-    </ul>
-</main>
+    <?php include '../view/_header.php' ?>
+
+    <main>
+
+
+        <ul>
+            <li>id : <?= $listNavire[0]['id'] ?></li>
+            <li>numeroIMO : <?= $listNavire[0]['numeroIMO'] ?></li>
+            <li>nom : <?= $listNavire[0]['nom'] ?></li>
+            <li>idTypeNavire : <?= $listNavire[0]['idTypeNavire'] ?></li>
+        </ul>
+    </main>
 </body>
+
 </html>
