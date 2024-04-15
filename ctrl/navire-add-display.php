55da880b052e2855e3a5c28ea09570d247844982
HTR <traorehamza407@gmail.com>
2024-04-15 10:54:28 +0200

    [New] Modification du fichier navire

---
+++ ctrl/navire-add-display.php
@@ -0,0 +1,23 @@
+<?php
+
+// Définit les clés de dictionnaire de la page
+$pageTitle = 'Ajouter un navire';
+
+// Liste les Navires disponibles
+// - Ouvre une connexion à la Base de données
+
+// Ouvre une connexion à la BDD
+include '../cfg/db.php';
+include '../model/lib/db.php';
+$dbConnection = getConnection($dbConfig);
+
+// - Prépare la requête
+$query = 'SELECT typeNavire.id, typeNavire.nom';
+$query .= ' FROM typeNavire';
+$statement = $dbConnection->prepare($query);
+// - Exécute la requête
+$successOrFailure = $statement->execute();
+$listTypeNavire = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// Rends la vue
+include '../view/navire-add.php';
---
+++ view/navire-add.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageTitle ?></title>
+    <link rel="stylesheet" href="/asset/css/style.css">
+</head>
+
+<body>
+
+    <?php include '../view/_header.php' ?>
+
+    <main>
+
+        <form action="/ctrl/navire-add.php" method="post">
+
+            <!-- numeroIMO -->
+            <div>
+                <label for="label">Numero IMO</label>
+                <input type="text" name="numeroIMO" id="numeroIMO">
+            </div>
+
+            <!-- Nom -->
+            <div>
+                <label for="code">Nom</label>
+                <input type="text" name="nom" id="nom">
+            </div>
+
+            <!-- idTypeNavire -->
+            <div>
+                <label for="typeNavire">Type de navire</label>
+                <select name="idTypeNavire" id="typeNavire">
+                    <?php foreach ($listTypeNavire as $typeNavire) { ?>
+
+                        <option value="<?= $typeNavire['id'] ?>"><?= $typeNavire['nom'] ?></option>
+                    <?php } ?>
+                </select>
+            </div>
+
+            <div class="submit">
+                <button type="submit">Valider</button>
+            </div>
+        </form>
+    </main>
+
+    <?php include '../view/_footer.php' ?>
+</body>
+
+</html>
