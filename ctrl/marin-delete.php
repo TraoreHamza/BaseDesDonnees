378153ebaeec39dc1a5678721bdcdc71086ce3a7
HTR <traorehamza407@gmail.com>
2024-04-15 14:00:00 +0200

    [new] ajouter un nouveau fichier

---
+++ ctrl/marin-delete.php
@@ -0,0 +1,24 @@
+<?php
+
+// supprimer un Marin
+
+// Lis les informations depuis la réquête HTTP (id du Marin)
+ $idMarin = $_GET['id'];
+
+// Ouvre une connexion à la Base de données
+include '../cfg/db.php';
+include '../model/lib/db.php';
+$dbConnection = getConnection($dbConfig);
+
+// - Prépare la requête
+$query .= 'DELETE';
+$query .= ' FROM `marin`';
+$query .= ' WHERE marin.id = :idMarin';
+$statement = $dbConnection->prepare($query);
+$statement->bindParam(':idMarin' , $idMarin);
+
+// - Exécute la requête
+$successOrFailure = $statement->execute();
+
+// Redirige vers la liste des Marins
+header('Location: ' . '/ctrl/marin-list.php');
